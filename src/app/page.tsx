b003cf3d696a01c31f0c224629cb637ae3beb5bf
Anand Shekhawat <anandsingh215@yahoo.com>
2023-05-23 23:08:55 +0530

    rearrange menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,8 @@ export default function Home() {
       <nav className={Styles.mainnav}>
         <ul>
           <Link href="/"><li>Home</li></Link>
-          <Link href="/about"><li>About</li></Link>
           <Link href="/blog"><li>Blog</li></Link>
+          <Link href="/about"><li>About</li></Link>
           <Link href="/contact"><li>Contact</li></Link>
         </ul>
       </nav>
