130c796691cb50e6883b67c95439add50191fdfa
Anand Shekhawat <anandsingh215@yahoo.com>
2025-05-10 14:19:53 +0530

    ff

--- src/components/GestureLayout.tsx
+++ src/components/GestureLayout.tsx
@@ -4,7 +4,7 @@ import { useRouter, usePathname } from "next/navigation";
 import { ReactNode, useRef, useState, useEffect } from "react";
 import PageWrapper from "./PageWrapper";
 
-const routes = ["/", "/about", "/content", "/gdrive", "/contact"];
+const routes = ["/", "/article", "/content", "/gdrive", "/about", "/contact"];
 
 export default function GestureLayout({ children }: { children: ReactNode }) {
   const router = useRouter();
