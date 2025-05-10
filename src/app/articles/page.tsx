cdb4a4fa19c8559fa5e864ffbd129c444dff41ef
Anand Shekhawat <anandsingh215@yahoo.com>
2025-05-10 14:47:40 +0530

    mdx

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -5,10 +5,10 @@ export default function MdxIndexPage() {
   const files = getAllMdxFiles();
 
   return (
-    <ul className="grid gap-x-8 gap-y-0 font-[family-name:var(--font-geist-sans)] text-center">
+    <ul className="grid sm:grid-cols-2 sm:w-screen gap-x-8 gap-y-0 font-[family-name:var(--font-geist-sans)]">
       {files.map((file, index) => (
-        <li key={file.slug} className="text-4xl font-bold mb-4 gap-2">
-          <span className="text-gray-400 w-6 text-right">{index + 1}.</span>
+        <li key={file.slug} className="text-4xl font-bold mb-4 flex gap-2">
+          <span className="text-gray-400 w-8 text-right">{index + 1}.</span>
           <Link
             href={`/articles/${file.slug}`}
             className="text-blue-600 hover:underline"
