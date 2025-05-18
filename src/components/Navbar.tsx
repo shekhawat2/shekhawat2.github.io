599cc9ccc4628dacb8627a8fcf98ecba54d1d6fd
Anand Shekhawat <anandsingh215@yahoo.com>
2025-05-18 12:06:45 +0530

    up

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,8 @@ import Link from "next/link";
 export default function Navbar() {
   return (
     <nav className="w-full px-4 py-3 flex justify-between items-center bg-white dark:bg-black shadow-md">
-      <div className="text-xl font-bold">Anand</div>
+      <Link className="text-xl font-bold" href="/">Anand</Link>
       <div className="space-x-4">
-        <Link href="/">Home</Link>
         <Link href="/articles">Articles</Link>
         <Link href="/content">Content</Link>
         <Link href="/gdrive">Gdrive</Link>
